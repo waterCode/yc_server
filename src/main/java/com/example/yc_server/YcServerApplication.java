7042288191a01b9c00c4a9f9cb2218d2d8ac0ae3
zmc <756343483@qq.com>
2018-03-18 22:56:20 +0800

    Initial commit

---
+++ src/main/java/com/example/yc_server/YcServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.yc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YcServerApplication.class, args);
+	}
+}

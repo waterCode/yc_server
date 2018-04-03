1998a1bfe7d3130b22cd648aed90d651ca1c2e14
zmc <756343483@qq.com>
2018-04-03 18:12:15 +0800

    彻底清除安全代码

--- src/main/java/com/example/yc_server/domain/SysUser.java
+++ src/main/java/com/example/yc_server/domain/SysUser.java
@@ -1,8 +1,6 @@
 package com.example.yc_server.domain;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import javax.persistence.*;
 import java.util.ArrayList;

ee92463d8182d1814066f71d9747d53d907769be
zmc <756343483@qq.com>
2018-05-13 12:50:14 +0800

    增加成员列表

--- src/main/java/com/example/yc_server/controller/SecureController.java
+++ src/main/java/com/example/yc_server/controller/SecureController.java
@@ -2,6 +2,7 @@ package com.example.yc_server.controller;
 
 
 import com.example.yc_server.domain.SysUser;
+import com.example.yc_server.repository.RegisterRepository;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/secure")
 public class SecureController {
 
-
+    @Autowired
+    private RegisterRepository registerRepository;
 
 
     @GetMapping("/users")
-    public String getUser(HttpServletRequest request){
+    public  List<SysUser> getUser(HttpServletRequest request){
         //只有管理者才有权限
+        List<SysUser> all=null;
         Claims claims = (Claims) request.getAttribute("claims");
         String userName = claims.getSubject();
+        if(userName.equals("admin")){
+            //是管理员，则返回成员列表
+            all = registerRepository.findAll();
+            Iterator<SysUser> iterator = all.iterator();
+            while (iterator.hasNext()){
+                SysUser next = iterator.next();
+                if(next.getUserName().equals("admin")){
+                    iterator.remove();
+                }
+            }
 
-        return userName;
+        }
+        return all;
     }
 
     @PostMapping("/joinUs")
